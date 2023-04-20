bbfd1a75f968d390a88d09355d530ba381a8b0b1
Snehal Navale <navalesnehal21@gmail.com>
2023-04-20 19:31:13 +0530

    server update

--- server.js
+++ server.js
@@ -10,35 +10,30 @@ function register(event){
     // var hotstarusers={uname:name,uemail:email,upassword:password,uconfirmpassword:confirmpassword};
     // console.log(hotstarusers);
     if(name && email && password && confirmpassword){
-        if(password>=8 && confirmpassword>=8){
+        if(password.length>=8 && confirmpassword.length>=8){
             if(password==confirmpassword){
-                var hotstaruserarray=JSON.parse(localStorage.getItem("hotstarusers")) || [];
+                var harray=JSON.parse(localStorage.getItem("hotstarusers")) || [];
                 // var hotstaruserarray=[];
                 var flagforemail=false;
-                for(var i=0;i<=hotstaruserarray.length;i++){
-                    console.log(hotstaruserarray.length);
-                    console.log(hotstaruserarray[i],"single user")
-                    console.log(hotstaruserarray,"areray");
-                    // console.log(hotstarusersdata,"object")
-                    if(hotstaruserarray[i].uemail == email){
-                        console.log(hotstaruserarray[i].uemail);
-                        console.log(email);
+                for(var i=0;i<harray.length;i++){
+                    if(harray[i].uemail == email){
                         flagforemail=true;
                     }
                 }
-                if(flagforemail==true){
+                if(flagforemail=false){
                     alert("email already exist");
                 }
                 else{
-                    var hotstarusersdata={uname:name,uemail:email,upassword:password,uconfirmpassword:confirmpassword};
-                    hotstaruserarray.push(hotstarusersdata);
-                    console.log(hotstaruserarray)
-                    localStorage.setItem("hotstarusers",JSON.stringify(hotstaruserarray))
+                    var hotstar={uname:name,uemail:email,upassword:password,uconfirmpassword:confirmpassword};
+                    harray.push(hotstar);
+                    // console.log(harray)
+                    localStorage.setItem("hotstarusers",JSON.stringify(harray))
                     alert("Registered Successfully")
                     document.getElementById("hname").value=''
                     document.getElementById("hemail").value=''
                     document.getElementById("hpassword").value=''
                     document.getElementById("hconfirmpassword").value=''
+                    window.location.href="./login.html"
                 }
             }
             else{
@@ -53,4 +48,40 @@ function register(event){
     else{
         console.log("all fields are required.")
     }
+}
+
+function login(event){
+    event.preventDefault();
+
+     var lemail=document.getElementById("hlemail").value;
+     var lpassword=document.getElementById("hlpassword").value;
+     var loginuser={}
+    //  console.log(lemail,lpassword)
+    if(lemail && lpassword){
+        var hlarray=JSON.parse(localStorage.getItem("hotstarusers"))
+        var flagforlogin=false;
+        console.log(hlarray)
+        for(var i=0;i<hlarray.length;i++){
+            if(hlarray[i].uemail==lemail && hlarray[i].upassword==lpassword){
+                flagforlogin=true;
+                loginuser=hlarray[i];
+            }
+        }
+        if(flagforlogin==true){
+            localStorage.setItem("hloginuser",JSON.stringify(loginuser));
+            alert("logged in Successfully")
+            window.location.href="./home.html";
+            document.getElementById("hlemail").value=''
+            document.getElementById("hlpassword").value=''
+        }
+        else{
+            alert("credentials not matched.")
+            
+        }
+
+    }
+    else{
+        console.log("both fields are required.")
+    }
+
 }
